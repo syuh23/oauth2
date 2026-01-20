8e93fe32d61bde54a0e9b0dabaf400b872a73b8c
syuh <seonghee5070@gmail.com>
2026-01-21 03:52:34 +0900

    db connected

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigFactory } from './config/ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: typeOrmConfigFactory,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
